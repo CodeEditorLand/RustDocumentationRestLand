5e83cde2e9dbdc2e9ecea39be5add50931540c44
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-01-22 18:57:59 +0200

    save
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

---
+++ doc/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["Library",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"Library/Struct/SWC/struct.CompilerConfig.html\" title=\"struct Library::Struct::SWC::CompilerConfig\">CompilerConfig</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"Library/Struct/SWC/struct.CompilerMetrics.html\" title=\"struct Library::Struct::SWC::CompilerMetrics\">CompilerMetrics</a>",0]]],["Rest",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"Rest/Struct/SWC/struct.CompilerConfig.html\" title=\"struct Rest::Struct::SWC::CompilerConfig\">CompilerConfig</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"Rest/Struct/SWC/struct.CompilerMetrics.html\" title=\"struct Rest::Struct::SWC::CompilerMetrics\">CompilerMetrics</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[632,618]}
